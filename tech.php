f423a6f796681f2083d02993581ec25e46d75bda
Yuzuno-Sakae <166361050+Yuzuno-Sakae@users.noreply.github.com>
2024-04-11 18:10:19 +0900

    Add files via upload

---
+++ tech.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>価格計算</title>
+</head>
+<body>
+    <h2>
+        桃とイチゴの価格をランダムで生成し、最高値・最安値・平均価格を表示します。
+    </h2>
+    <p>桃は200~300円、イチゴは400~500円の範囲で価格が変動します。<br>
+        ランダムな価格は15個生成されます。
+    </p>
+    <form action= "tech2.php" method= "post">
+        <input type= "submit" name= "submit" value="計算" />
+</body>
+</html>
